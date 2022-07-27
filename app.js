9bd766596c9fc41428f1d6b80218d9858352f38d
ITsverkunov <tsverkunovivan@gmail.com>
2022-07-27 17:20:37 +0300

    feat: Added controller users

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const  {errors} = require('celebrate')
 
 const {PORT = 3000} = process.env
 
 const app = express()
+
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb')
   // eslint-disable-next-line no-console
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,42 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+
+const {MONGO_DUPLICATE_ERROR_CODE} = require("../utils/constants");
+
+const DataError = require('../errors/DataError')
+const DuplicateError = require("../errors/DuplicateError");
 
 module.exports.createUser = (req, res, next) => {
+  const {
+    name,
+    email,
+    password,
+  } = req.body
 
+  if (!email || !password) {
+    throw new DataError('Не введен email или пароль.')
+  }
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+        name,
+        email,
+        password: hash
+      })
+    )
+    .then((user) => {
+      res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email
+      })
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(new DataError('Пользователь с указанным _id не найден.'));
+      }
+      if (err.code === MONGO_DUPLICATE_ERROR_CODE) {
+        return next(new DuplicateError('Email занят'));
+      }
+      return next(err);
+    })
 }
