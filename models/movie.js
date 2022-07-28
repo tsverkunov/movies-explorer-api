5cb8a1f8eee9217c3b755ae84bfd5ba0c83c19f4
ITsverkunov <tsverkunovivan@gmail.com>
2022-07-28 20:24:15 +0300

    feat: Added movieSchema and urlValidator

---
+++ models/movie.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const { urlValidation } = require('../utils/urlValidator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    validate: urlValidation,
+    required: true,
+  },
+  trailerLink: {
+    type: String,
+    validate: urlValidation,
+    required: true,
+  },
+  thumbnail : {
+    type: String,
+    validate: urlValidation,
+    required: true,
+  },
+  owner: {
+    required: true,
+  },
+  movieId: {
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
