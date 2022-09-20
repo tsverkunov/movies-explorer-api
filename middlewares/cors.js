f1c55db70bf3b0c8750f44600167068c78e615b1
ITsverkunov <tsverkunovivan@gmail.com>
2022-09-20 18:43:41 +0300

    fix: CORS method OPTIONS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -18,7 +18,6 @@ module.exports.cors = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
   }
 
   next();
