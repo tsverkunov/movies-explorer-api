85f883dd08ecf706bf694fe74ed3611dc8804bf1
ITsverkunov <tsverkunovivan@gmail.com>
2022-09-27 22:31:38 +0300

    fix: Whithout secure

--- controllers/users.js
+++ controllers/users.js
@@ -95,7 +95,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        secure: true,
+        // secure: true,
         sameSite: 'None',
       })
         .send({
--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,5 +2,5 @@ const rateLimit = require('express-rate-limit');
 
 module.exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
 });
